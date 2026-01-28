6658ed20aaef901dbab24372c2725d88d88c9bb0
kspark-sqisoft <ks.park@sqisoft.com>
2026-01-28 22:42:11 +0900

    테스트 shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider";
 
 export default function Home() {
   return (
-    <main className="p-5">
-      <Button variant="outline">Click Me</Button >
+    <main className="flex flex-col gap-5 p-10">
+      <Button className="mx-auto w-full max-w-xs">Click Me</Button >
+      <Slider
+        defaultValue={[75]}
+        max={100}
+        step={1}
+        className="mx-auto w-full max-w-xs"
+      />
     </main >
 
   );
